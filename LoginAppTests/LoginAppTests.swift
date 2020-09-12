aa42a1e6daf2fa6f7bedf588e4abac823fe00810
Evgheni Lisita <evghenii.lisita@gmail.com>
2020-09-12 17:29:36 +0200

    added tests

--- LoginAppTests/LoginAppTests.swift
+++ LoginAppTests/LoginAppTests.swift
@@ -11,24 +11,76 @@ import XCTest
 
 class LoginAppTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testFirstNameCapitalLetter() throws {
+        let viewModel = LoginViewModel()
+
+        viewModel.firstName = "Myvalidfname"
+        XCTAssertTrue(viewModel.firstName.isValidFirstName)
+
+        viewModel.firstName = "myinvalidfname"
+        XCTAssertFalse(viewModel.firstName.isValidFirstName)
+
+        viewModel.firstName = ""
+        XCTAssertFalse(viewModel.firstName.isValidFirstName)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testLastNameCapitalLetter() throws {
+        let viewModel = LoginViewModel()
+
+        viewModel.lastName = "Myvalidlname"
+        XCTAssertTrue(viewModel.lastName.isValidLastName)
+
+        viewModel.lastName = "myinvalidlname"
+        XCTAssertFalse(viewModel.lastName.isValidLastName)
+
+        viewModel.lastName = ""
+        XCTAssertFalse(viewModel.lastName.isValidLastName)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testValidEmailAddress() throws {
+        let viewModel = LoginViewModel()
+
+        viewModel.email = "my@email.com"
+        XCTAssertTrue(viewModel.email.isValidEmail)
+
+        viewModel.email = "myin@me.s"
+        XCTAssertFalse(viewModel.email.isValidEmail)
+
+        viewModel.email = ""
+        XCTAssertFalse(viewModel.email.isValidEmail)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    func testValidPhoneNumber() throws {
+        let viewModel = LoginViewModel()
 
+        /* Phone Positive:
+        +42 555.123.4567
+        +1-(800)-123-4567
+        +7 555 1234567
+        +7(926)1234567
+        (926) 1234567
+        +79261234567
+        926 1234567
+        9261234567
+        1234567
+        123-4567
+        123-89-01
+        495 1234567
+        469 123 45 67
+        89261234567
+        8 (926) 1234567
+        926.123.4567
+        415-555-1234
+        650-555-2345
+        (416)555-3456
+        202 555 4567
+        4035555678
+        1 416 555 9292**/
+
+        viewModel.phoneNumber = "+7(926)1234567"
+        XCTAssertTrue(viewModel.phoneNumber.isValidPhoneNumber)
+
+        viewModel.phoneNumber = "234ff2345"
+        XCTAssertFalse(viewModel.phoneNumber.isValidPhoneNumber)
+    }
 }
