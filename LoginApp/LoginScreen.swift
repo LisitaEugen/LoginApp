a9ef7348be6ef9f1e4fc22c6858b474e2d746760
Evgheni Lisita <evghenii.lisita@gmail.com>
2020-09-12 13:29:15 +0200

    add valid icon

--- LoginApp/LoginScreen.swift
+++ LoginApp/LoginScreen.swift
@@ -13,17 +13,44 @@ struct LoginScreen: View {
     
     var body: some View {
         VStack(alignment: .center, spacing: 20.0) {
-            TextField("First Name", text: $viewModel.firstName)
-            TextField("Last Name", text: $viewModel.lastName)
-            TextField("E-mail", text: $viewModel.email)
-            TextField("Phone number", text: $viewModel.phoneNumber)
+            ValidatedTextField("First Name", text: $viewModel.firstName, valid: true)
+            ValidatedTextField("Last Name", text: $viewModel.lastName)
+            ValidatedTextField("E-mail", text: $viewModel.email)
+            ValidatedTextField("Phone number", text: $viewModel.phoneNumber)
             Spacer()
         }.padding(EdgeInsets(top: 50, leading: 20, bottom: 30, trailing: 20))
     }
 }
 
+struct ValidatedTextField: View {
+    var name: String
+    @Binding var text: String
+    var valid: Bool
+
+    init(_ name: String, text: Binding<String>, valid: Bool = false) {
+        self.name = name
+        self._text = text
+        self.valid = valid
+    }
+
+    var body: some View {
+        HStack{
+            TextField(name, text: $text)
+            if valid {
+                Image(systemName: "checkmark.circle.fill")
+                .foregroundColor(.green)
+            } else {
+                Image(systemName: "exclamationmark.triangle.fill")
+                .foregroundColor(.yellow)
+            }
+        }
+    }
+}
+
+
+
 struct LoginScreen_Previews: PreviewProvider {
     static var previews: some View {
-        LoginScreen()
+        LoginScreen().environmentObject(LoginViewModel())
     }
 }
